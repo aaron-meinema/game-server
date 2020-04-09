cb7d85522dd5cdb222920fe4e3783898cf42bc34
roberthaitsma <robert.haitsma@gmail.com>
2020-04-09 14:47:09 +0200

    added coupon to cart function

--- oauth_web_api.py
+++ oauth_web_api.py
@@ -1,15 +1,95 @@
 import requests
+import json
 from requests_oauthlib import OAuth1
-from urllib.parse import urlparse
 
 resource_owner_key = 'rtxojvqglcd2m4f8n3pg6nhi1dviil00'
 resource_owner_secret = 'bhzrf9y224nc2rlonf5z6urr0499n8ih'
 client_key = 'm748bxohupsrer5hqvaynfkhy8nlpyz8'
 client_secret = 'zz2z4zowvgyujqf38wcnzb0koqch7rx4'
 
-request_token_url = 'http://127.0.0.1:8080/rest/V1/products/99212213'
+base_request_url = 'http://127.0.0.1:8080/rest/V1/'
 
-oauth = OAuth1(client_key, client_secret=client_secret, resource_owner_key=resource_owner_key, resource_owner_secret=resource_owner_secret)
-r = requests.get(url=request_token_url, auth=oauth)
-# r.content
-print(r.content)
+oauth = OAuth1(client_key, client_secret=client_secret, resource_owner_key=resource_owner_key,
+               resource_owner_secret=resource_owner_secret)
+
+# request_token_url = 'http://127.0.0.1:8080/rest/V1/coupons/generate'
+# request_token_url = 'http://127.0.0.1:8080/rest/V1/guest-carts/GDhzkXMSQVYT9kchcpc3QcCjkDF3txUD/coupons/40U2OX51368B'
+
+x = {
+    "couponSpec":
+    {
+        "rule_id": 1,
+        "format": "string",
+        "quantity": 1,
+        "length": 0,
+        "prefix": "string",
+        "suffix": "string",
+        "delimiter_at_every": 0,
+        "delimiter": "string",
+        "extension_attributes": { }
+    }
+}
+
+
+def sendCouponRequest():
+    code = {
+        "couponSpec": {
+            "rule_id": 97,
+            "format": "",
+            "quantity": 1,
+            "length": 12,
+            "prefix": "",
+            "suffix": "",
+            "delimiter_at_every": 0,
+            "delimiter": "",
+            "extension_attributes": {}
+        }
+    }
+
+    ccode = {
+      "giftCardAccountData": {
+        "gift_cards": [
+          "40U2OX51368B"
+        ],
+        "gift_cards_amount": 0,
+        "base_gift_cards_amount": 0,
+        "gift_cards_amount_used": 0,
+        "base_gift_cards_amount_used": 0,
+        "extension_attributes": {}
+      }
+    }
+
+    coupon_link = 'coupons/generate'
+    request_token_url = base_request_url + coupon_link
+
+    # convert into JSON:
+    y = json.dumps(code)
+
+    r = requests.post(url=request_token_url, auth=oauth, data=y, headers={'Content-Type': 'application/json'})
+
+    print(r.content)
+
+    coupon = r.content.decode('utf-8')
+
+    if coupon is None:
+        return ""
+    else:
+        return coupon[2:-2]
+
+
+def addCouponToCart(cart_id, coupon_code):
+    if cart_id.isdigit():
+        cart_link = 'cart/' + int(cart_id)
+    else:
+        cart_link = 'guest-carts/' + cart_id
+    coupon_link = '/coupons/' + coupon_code
+
+    request_token_url = base_request_url + cart_link + coupon_link
+
+    r = requests.put(url=request_token_url, auth=oauth, headers={'Content-Type': 'application/json'})
+
+    print(r.content)
+
+
+# sendCouponRequest()
+# addCouponToCart('GDhzkXMSQVYT9kchcpc3QcCjkDF3txUD', '40U2OX51368B')
--- server.py
+++ server.py
@@ -1,8 +1,14 @@
+import oauth_web_api
+import json
+
 from flask import request, url_for
+from flask_cors import CORS
 from flask_api import FlaskAPI, status, exceptions
+from datetime import datetime
 
 app = FlaskAPI(__name__)
-
+CORS(app)
+m2_api = oauth_web_api
 high_score = 0
 
 
@@ -32,9 +38,37 @@ def category_game_finished(subcategory, score):
         # subcategory moet naar de database gecheckt worden, vervolgens de origin controleren. dan user etc.
         return status.HTTP_405_METHOD_NOT_ALLOWED
     else:
-        if score > high_score:
+        if score > 100:
             high_score = score
-        return "high score is " + str(high_score) + " your score is: " + str(score), status.HTTP_200_OK
+            coupon = m2_api.sendCouponRequest()
+            if coupon == "":
+                return '-1', status.HTTP_200_OK
+            else:
+                return str(coupon), status.HTTP_201_CREATED
+        else:
+            return '-1', status.HTTP_200_OK
+
+
+@app.route("/<string:subcategory>/<int:score>/<string:cart_id>", methods=['POST'])
+def category_game_finished_add_to_cart(subcategory, score, cart_id):
+    global high_score
+    if request.environ['HTTP_ORIGIN'] is not None:
+        print(request.environ['HTTP_ORIGIN'])
+
+    if subcategory != 'test':
+        # subcategory moet naar de database gecheckt worden, vervolgens de origin controleren. dan user etc.
+        return status.HTTP_405_METHOD_NOT_ALLOWED
+    else:
+        if score > 100:
+            high_score = score
+            coupon = m2_api.sendCouponRequest()
+            if coupon == "":
+                return '-1', status.HTTP_200_OK
+            else:
+                m2_api.addCouponToCart(cart_id, coupon)
+                return str(coupon), status.HTTP_201_CREATED
+        else:
+            return '-1', status.HTTP_200_OK
 
 
 @app.route("/oauth", methods=['GET', 'POST', 'PUT'])
